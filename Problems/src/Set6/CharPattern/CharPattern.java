e0f4bc9c4c15db8f27e86e1f4f563b3f5fb5bcfa
mona-06 <moanariya1403@gmail.com>
2025-11-23 23:30:05 +0530

    Added Set6 Programs

---
+++ Problems/src/Set6/CharPattern/CharPattern.java
@@ -0,0 +1,18 @@
+package Set6.CharPattern;
+
+public class CharPattern {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+
+            char ch = 'A';
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
